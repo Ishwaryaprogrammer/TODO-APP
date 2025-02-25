f466a7f8b8c464e8261de04df7d5a42dfd5e1ced
Ishwarya Suresh <109035230+Ishwaryaprogrammer@users.noreply.github.com>
2025-02-25 23:10:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,8 @@ app.use(cors());
 app.use(express.static('public'));
 // Connect to MongoDB (use environment variable for deployment)
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/todo-app';
+mongoose.connect(process.env.MONGO_URI)
 
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
